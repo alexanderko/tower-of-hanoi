53e8505459aa02cacfd3c6e1ac5e2785965016eb
Alexander Kotov <alexanderko@interlink-ua.com>
2018-08-08 03:11:48 +0300

    restore game state after page reload

--- src/store.js
+++ src/store.js
@@ -4,7 +4,12 @@ import rootReducer from './reducers'
 
 const store = createStore(
   rootReducer,
+  localStorage.state && JSON.parse(localStorage.state), 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  localStorage.setItem('state', JSON.stringify(store.getState()))
+})
 
 export default store
